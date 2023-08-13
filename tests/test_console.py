2c6a4b596b5fe3a3922a446983a27790ee07e646
Mariga <mariga.bn@gmail.com>
2023-08-13 05:16:14 -0700

    refractor: test console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -3,7 +3,8 @@ from unittest.mock import patch
 from io import StringIO
 from console import HBNBCommand
 
-class TestConsoleShowCommand(unittest.TestCase):
+
+class TestConsoleCommands(unittest.TestCase):
 
     def setUp(self):
         self.console = HBNBCommand()
@@ -26,18 +27,20 @@ class TestConsoleShowCommand(unittest.TestCase):
         self.assertEqual(mock_stdout.getvalue().strip(), expected_output)
 
     @patch('sys.stdout', new_callable=StringIO)
-    def test_show_invalid_class(self, mock_stdout):
-        # Test 'show' with invalid class name
-        self.console.onecmd("show InvalidClass 123")
-        expected_output = "** class doesn't exist **"
+    def test_update_valid(self, mock_stdout):
+        # Create an instance to be updated
+        self.console.onecmd("create User")
+        obj_id = mock_stdout.getvalue().strip()
+
+        # Reset mock_stdout for next capture
+        mock_stdout.truncate(0)
+
+        # Test 'update' command
+        self.console.onecmd(f"update User {obj_id} name John")
+        updated_instance = self.console.classlist['User'](id=obj_id, name="John")
+        expected_output = "[User] ({}) {}".format(obj_id, str(updated_instance))
         self.assertEqual(mock_stdout.getvalue().strip(), expected_output)
 
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_show_invalid_id(self, mock_stdout):
-        # Test 'show' with invalid object ID
-        self.console.onecmd("show User InvalidID")
-        expected_output = "** no instance found **"
-        self.assertEqual(mock_stdout.getvalue().strip(), expected_output)
 
 if __name__ == "__main__":
     unittest.main()
